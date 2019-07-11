3c41dece5b4702f50b90fe38314ed6c381a330e7
horaleon1 <horaleon1@gmail.com>
2019-07-11 13:29:16 -0500

    "monster game"

--- course/otro.js
+++ course/otro.js
@@ -29,38 +29,100 @@
 
 // });
 
-new Vue ({
-  el: '#app',
-  data:{
-    title : "Hello World 2",
-    link: 'http://google.com',
-    count: 0,
-    x: 0,
-    y: 0,
-    name: "hola",
-    atRed:false
-    
-},
-methods: {
-  sayHello: function(){
-    this.title = 'Hello!'
-    return this.title;
+// new Vue ({
+//   el: '#app',
+//   data:{
+//     title : "Hello World 2",
+//     link: 'http://google.com',
+//     count: 0,
+//     x: 0,
+//     y: 0,
+//     name: "hola",
+//     atRed:false
+
+// },
+// methods: {
+//   sayHello: function(){
+//     this.title = 'Hello!'
+//     return this.title;
+//   },
+//   increase: function(step,event){
+//      this.count += step;
+//   },
+//   updateCoordinates: function(event){
+//      this.x = event.clientX;
+//      this.y = event.clientY;
+//   },
+//   // dummy: function(){
+//   //   event.stopPropagation();
+//   // }
+//   alertMe: function(){
+//    alert('Alert!');
+//   }
+// }
+// });
+
+// new Vue({
+
+//   el:"#app",
+//   data:{
+//     show:true,
+//     ingredients:['meat','fruit','cookies'],
+//     persons:[
+//       {name:'Max', age: '27',color:'red'},
+//       {name: 'Anna',age: '33', color: 'blue'}
+//     ],
+
+//   }
+// })
+
+new Vue({
+  el: "#app",
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false
   },
-  increase: function(step,event){
-     this.count += step;
-  },
-  updateCoordinates: function(event){
-     this.x = event.clientX;
-     this.y = event.clientY;
-  },
-  // dummy: function(){
-  //   event.stopPropagation();
-  // }
-  alertMe: function(){
-   alert('Alert!');
+  methods: {
+    startGame: function() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function() {
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+
+      if(this.checkWin()){
+        return;
+      }
+      
+      damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {},
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm("Yo won! new game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You lost! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    }
   }
-} 
 });
-
-
-
